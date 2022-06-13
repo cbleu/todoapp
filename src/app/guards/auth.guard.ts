843c3afb1df609c9084aeb6504fe1cac2ea03b2b
Cesar Jacquet <cesar.jacquet@gmail.com>
2022-06-13 16:43:46 +0400

    guard, service, interceptor

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,28 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanLoad, Router } from '@angular/router';
+import { filter, map, Observable, take } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canLoad(): Observable<boolean> {
+    console.log('AuthGuard ~ canLoad');
+    return this.authService.isAuthenticated.pipe(
+      filter((val) => val !== null), // Filter out initial Behaviour subject value
+      take(1), // Otherwise the Observable doesn't complete!
+      map((isAuthenticated) => {
+        console.log('AuthGuard ~ canLoad ~ isAuthenticated:', isAuthenticated);
+        if (isAuthenticated) {
+          return true;
+        } else {
+          this.router.navigateByUrl('/login');
+          return false;
+        }
+      })
+    );
   }
 }
