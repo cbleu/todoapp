13f5a0f52cec6479552dc3b2d54e4f656f2a9e49
Cesar Jacquet <cesar.jacquet@gmail.com>
2022-06-13 16:50:22 +0400

    auth JWT ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,41 +10,26 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () =>
-      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+    loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
   {
     path: 'lost-password',
-    loadChildren: () =>
-      import('./pages/lost-password/lost-password.module').then(
-        (m) => m.LostPasswordPageModule
-      ),
-  },
-  {
-    path: 'register',
-    loadChildren: () =>
-      import('./pages/register/register.module').then(
-        (m) => m.RegisterPageModule
-      ),
+    loadChildren: () => import('./pages/lost-password/lost-password.module').then((m) => m.LostPasswordPageModule),
   },
   {
     path: 'home',
-    loadChildren: () =>
-      import('./pages/home/home.module').then((m) => m.HomePageModule),
+    loadChildren: () => import('./pages/home/home.module').then((m) => m.HomePageModule),
     canLoad: [AuthGuard],
   },
   {
     path: 'detail',
-    loadChildren: () =>
-      import('./pages/detail/detail.module').then((m) => m.DetailPageModule),
+    loadChildren: () => import('./pages/detail/detail.module').then((m) => m.DetailPageModule),
     canLoad: [AuthGuard],
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
